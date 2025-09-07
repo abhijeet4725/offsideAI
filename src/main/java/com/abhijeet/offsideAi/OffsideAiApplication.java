27d27a75a38caf9523038f6e6afdb4bdd278eeab
Abhijeet Singh <abhijeetcs4725@gmail.com>
2025-09-08 00:04:53 +0530

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/abhijeet/offsideAi/OffsideAiApplication.java
@@ -0,0 +1,13 @@
+package com.abhijeet.offsideAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OffsideAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OffsideAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abhijeet/offsideAi/OffsideAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhijeet.offsideAi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OffsideAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
